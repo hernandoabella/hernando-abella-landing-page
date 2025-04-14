05f0d0c5f5104dd35649d8f949876f8fbd91f0d9
Hernando Abella <hernandoabella@gmail.com>
2025-04-13 22:01:57 -0500

    Update Contact.tsx

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const ContactSection = () => {
   return (
-    <section className="py-32 bg-[#141414]" id="contact">
+    <section className="bg-[#141414]" id="contact">
       <div className="container text-center">
         <h2 className="font-extrabold text-transparent text-3xl sm:text-4xl xl:text-5xl bg-clip-text bg-gradient-to-b from-slate-500 via-slate-100 to-slate-500 bg-transparent h-full mb-6">
           Contact Me
