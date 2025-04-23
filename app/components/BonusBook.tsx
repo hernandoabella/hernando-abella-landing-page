e19d6c6dbd53580abc75953add6b13c6ca620840
Hernando Abella <hernandoabella@gmail.com>
2025-04-22 23:46:14 -0500

    Update BonusBook.tsx

--- app/components/BonusBook.tsx
+++ app/components/BonusBook.tsx
@@ -5,8 +5,22 @@ import BonusBtn from "./BonusBtn";
 
 const DownloadBonusBook = () => {
   return (
-    <section className=" dark:bg-[#141414]">
-      <div className="mx-auto text-center py-20">
+    <section className="relative">
+      {/* Background Video */}
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/tech.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      
+      <div className="mx-auto text-center p-20 relative z-10">
+        <h2 className="mb-5 font-extrabold text-transparent text-3xl sm:text-4xl xl:text-5xl bg-clip-text bg-gradient-to-r from-green-100 via-green-300 to-green-500 bg-transparent h-full">
+          Bonus Books!
+        </h2>
         <BonusBtn />
       </div>
     </section>
