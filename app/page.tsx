340d36e0f7dcff17c75df5bc383215fef56ddc6d
Hernando Abella <hernandoabella@gmail.com>
2025-04-22 19:47:07 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Header />
         <Hero />
         <About />
-        <SideProjects />
+        {/* <SideProjects /> */}
         <Services />
         <Testimonials />
         <CTA />
