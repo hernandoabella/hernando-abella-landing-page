8ebc964d299bc2cb145a5685c3e38bbaeb84287a
Hernando Abella <hernandoabella@gmail.com>
2025-04-22 22:08:31 -0500

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -30,28 +30,36 @@ const AboutMe = () => {
         {/* Skills Circle */}
         <div className="relative w-full md:w-1/2 flex justify-center md:justify-end">
           <div className="relative w-[300px] sm:w-[400px] md:w-[500px] h-[300px] sm:h-[400px] md:h-[500px] flex items-center justify-center">
-            <div className="absolute inset-0 flex items-center justify-center">
-              {/* Icon Group */}
-              <i className="devicon-html5-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-6 left-12 animate-pulse" />
-              <i className="devicon-css3-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-16 right-6 animate-pulse" />
-              <i className="devicon-sass-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-28 left-1/2 transform -translate-x-1/2 animate-pulse" />
-              <i className="devicon-javascript-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute bottom-6 left-1/4 animate-pulse" />
-              <i className="devicon-typescript-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute bottom-12 right-10 animate-pulse" />
-              <i className="devicon-react-original text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-4 right-1/4 animate-pulse" />
-              <i className="devicon-nodejs-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-32 left-1/4 animate-pulse" />
-              <i className="devicon-php-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute bottom-4 right-6 animate-pulse" />
-              <i className="devicon-java-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-1/2 left-1/4 animate-pulse" />
-              <i className="devicon-csharp-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute bottom-1/4 left-1/4 animate-pulse" />
-              <i className="devicon-mongodb-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-1/4 right-20 animate-pulse" />
-              <i className="devicon-mysql-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute bottom-10 right-1/4 animate-pulse" />
-              <i className="devicon-git-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-1/4 left-1/4 animate-pulse" />
-              <i className="devicon-github-original text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-1/2 right-10 animate-pulse" />
-              <i className="devicon-docker-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute bottom-6 left-10 animate-pulse" />
-              <i className="devicon-figma-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-3 left-3 animate-pulse" />
-              <i className="devicon-tailwindcss-plain text-3xl sm:text-4xl md:text-5xl text-green-500 absolute top-36 right-1/4 animate-pulse" />
+            <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-green-700 via-green-800 to-green-900">
+              {/* Icons Container */}
+              <div className="grid grid-cols-4 gap-10 justify-items-center items-center">
+                <i className="devicon-figma-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-tailwindcss-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-html5-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-css3-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-javascript-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-typescript-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-react-original text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-nodejs-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-php-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-java-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-csharp-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-mongodb-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-mysql-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-git-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-github-original text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-docker-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-postgresql-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-firebase-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-vuejs-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-express-original text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-graphql-plain text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+                <i className="devicon-nextjs-original text-5xl sm:text-6xl text-green-500 hover:text-green-300 transition duration-300 ease-in-out transform hover:scale-125" />
+              </div>
             </div>
           </div>
         </div>
+
       </div>
     </section>
   );
