2501c51e83e338995c3e8437ccdfa35c78a74fd4
Hernando Abella <hernandoabella@gmail.com>
2025-04-23 20:05:05 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -53,22 +53,16 @@ const Hero = () => {
         </div>
 
         {/* CTA Buttons */}
-        <div className="mt-6 flex gap-10 flex-wrap justify-center">
+        <div className="mt-6 flex gap-10 justify-center">
           <a href="https://portfolio-hernandoabella.vercel.app/" target="_blank">
             <div className="relative group">
-            <button className="button">
-  <span>Portfolio</span>
-</button>
-
+              <button className="group relative h-12 overflow-hidden overflow-x-hidden rounded-3xl bg-green-950 px-8 py-2 text-gray-200"><span className="relative z-10">Hire Me</span><span className="absolute inset-0 overflow-hidden rounded-md"><span className="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-green-500  transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span></span></button>
             </div>
           </a>
 
           <a href="#contact">
             <div className="relative group">
-            <button className="button">
-  <span>Hire me</span>
-</button>
-
+              <button className="group relative h-12 overflow-hidden overflow-x-hidden rounded-3xl border-2 border-green-950 px-8 py-2 text-gray-200"><span className="relative z-10">Portfolio</span><span className="absolute inset-0 overflow-hidden rounded-md"><span className="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-green-500 transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span></span></button>
             </div>
           </a>
         </div>
