661b897c9db7b6c70141ee5609c06a69fd58e99b
hernandoabella <hernandoabella@gmail.com>
2026-01-21 23:57:47 -0500

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Share_Tech_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 
 const shareTechMono = Share_Tech_Mono({ subsets: ["latin"], weight: "400" });
 
@@ -24,14 +24,20 @@ export default function RootLayout({
         />
       </head>
       <body className={shareTechMono.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="default"
-          enableSystem
-          disableTransitionOnChange
+        <Script
+          id="mailerlite-universal"
+          strategy="afterInteractive"
         >
-          {children}
-        </ThemeProvider>
+          {`
+            (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '2055858');
+          `}
+        </Script>
+        {children}
+
       </body>
     </html>
   );
