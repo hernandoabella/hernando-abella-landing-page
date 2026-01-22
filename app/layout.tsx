38b306a8607610cc6ac27b0356694780332b2ced
hernandoabella <hernandoabella@gmail.com>
2026-01-22 00:11:37 -0500

    newsletter added!

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,20 +24,29 @@ export default function RootLayout({
         />
       </head>
       <body className={shareTechMono.className}>
+        {/* MailerLite Universal */}
         <Script
           id="mailerlite-universal"
           strategy="afterInteractive"
         >
           {`
-          (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-    ml('account', '2055858');
+            (function(w,d,e,u,f,l,n){
+              w[f]=w[f]||function(){
+                (w[f].q=w[f].q||[]).push(arguments);
+              },
+              l=d.createElement(e),
+              l.async=1,
+              l.src=u,
+              n=d.getElementsByTagName(e)[0],
+              n.parentNode.insertBefore(l,n);
+            })(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+
+            ml('account', '2055858');
           `}
         </Script>
+        {/* End MailerLite Universal */}
+        
         {children}
-
       </body>
     </html>
   );
